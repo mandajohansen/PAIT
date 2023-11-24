36531071cefba078695856a15f1b23f634a42b3a
Simsmeister <rr67wh@student.aau.dk>
2023-11-24 12:36:31 +0100

    Key and Drawer Interaction

---
+++ Assets/KeyTrigger.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class KeyTrigger : MonoBehaviour
+{
+
+    public bool isKeyInserted;
+    public GameObject drawerToInteract;
+    // Start is called before the first frame update
+    void Start()
+    {
+        drawerToInteract = GameObject.FindWithTag("Drawer");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Key"))
+        {
+            // Key entered the trigger zone
+            isKeyInserted = true;
+            drawerToInteract.GetComponent<XRGrabInteractable>().enabled = false;
+            Debug.Log("Key inserted!");
+        }
+    }
+}
