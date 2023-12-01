19944a2959775d07311686496410f2a3c8999887
Simsmeister <rr67wh@student.aau.dk>
2023-12-01 16:58:31 +0100

    KeyPad test

---
+++ Assets/Keypad/Scripts/DoorOpener.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+
+public class DoorOpener : MonoBehaviour
+{
+
+    public GameObject door;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DoorOpening()
+    {
+        door.GetComponent<XRGrabInteractable>().enabled = true;
+    }
+}
