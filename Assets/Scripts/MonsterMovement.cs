118d2910f2f3f04512c93c24bf29e8a241062798
mandajohansen <aljo21@student.aau.dk>
2023-12-03 21:42:16 +0100

    Update på outro

--- Assets/Scripts/MonsterMovement.cs
+++ Assets/Scripts/MonsterMovement.cs
@@ -7,34 +7,28 @@ public class MonsterMovement : MonoBehaviour
 
     public float waitTime = 5f;
     public bool timeToWalk = false;
-   //public float speed = 50f;
-  // public float upSpeed = 0.0015f;
-   
-   // void FixedUpdate()
-    //{
-        //GetComponent<Rigidbody2D>().AddForce(Vector3.up * upSpeed);
-    //}
+  
 
     public float speed = 2;
     
-    void Start()
+   /* void Start()
     {
         StartCoroutine(WalkTime());
     }
-    
+    */
     void Update()
 {
-    if (timeToWalk) 
-    {
+   // if (timeToWalk) 
+    //{
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
-    }
-    // Moves the object forward at two units per second.
+   // }
+
 }
 
-    IEnumerator WalkTime()
+    /*IEnumerator WalkTime()
     {
         yield return new WaitForSeconds(waitTime);
 
         timeToWalk = true;
-    }
+    }*/
 }
