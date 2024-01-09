4760bc616562f692b8cafb930e036e4d986cb058
Hamzamq4 <92908776+Hamzamq4@users.noreply.github.com>
2024-01-09 23:31:42 +0100

    Comments til LeverTrigger

--- Assets/Scripts/leverTrigger.cs
+++ Assets/Scripts/leverTrigger.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class LeverTrigger : MonoBehaviour
 {
+    // Define Variables
     GameObject chain;
     Rigidbody chainRigidbody;
     public AudioSource playSound;
@@ -14,12 +15,13 @@ public class LeverTrigger : MonoBehaviour
     {
         // Use GameObject.FindWithTag to find the chain by its tag
         chain = GameObject.FindWithTag("chains");
+        // Get the Chains Rigid Body
         chain.GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        
+        // If Chain is not null, i.e it is found, then set the chainRigidbody variable to the chain's rigidbody
         if (chain != null)
         {
             chainRigidbody = chain.GetComponent<Rigidbody>();
@@ -30,16 +32,25 @@ public class LeverTrigger : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        // If the collided object is a lever 2 then:
         if (other.CompareTag("Lever_2"))
         {
-            if(!hasTriggered)
-            { 
-            chainRigidbody.isKinematic = false;
-            chainRigidbody.useGravity = true;
-            playSound.Play();
-            hasTriggered = true;
-            Debug.Log("Lever Done");
+
+            if (!hasTriggered)
+            {
+                // If it has not triggered then:
+                // Set the chainRigidbody's isKinematic to false, so that it can move
+                // Set the chainRigidbody's useGravity to true, so that it can fall into position
+                // Play the sound
+                // Set hasTriggered to true
+                // Log "Lever Done"
+
+                chainRigidbody.isKinematic = false;
+                chainRigidbody.useGravity = true;
+                playSound.Play();
+                hasTriggered = true;
+                Debug.Log("Lever Done");
             }
-    }
+        }
     }
 }
